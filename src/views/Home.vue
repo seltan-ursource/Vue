2d8743f5008c4ef066eb0fcbcd77e572a2ab2eba
SeltanUrsource <seltanursource@gmail.com>
2020-04-17 18:36:52 +0530

    Bug fixing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,12 @@
   <div class="home">
     <h1>HP Laptops</h1>
     <div v-for="product in products" :key="product.id" class="product-details">
-      <div class="weather-stats">
+      <div class="Product-stats">
         <div>
           <span>{{product.name}}</span>
+        </div>
+        <div>
+          <img class="materialboxed" :src="product.image" width="200">
         </div>
         <div>
           <span class="Brand"><b>Brand:</b> {{product.brand}}</span>
@@ -18,7 +21,7 @@
         <div>
           <span class="Price"><b>Price:</b> {{product.price}}</span>
         </div>
-      </div>
+      </div><br>
   </div>
   </div>
 </template>
