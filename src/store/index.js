2f0a7928b40d3de4243856f6e4637c1a548db7db
SeltanUrsource <seltanursource@gmail.com>
2020-04-18 09:01:07 +0530

    WIP Learning

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    product: { id: 'abc123', name: 'Hewlett-Packard' },
+    categories: ['Desktop', 'Notebook', 'Gaming']
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters:{
+    categoryLeagth : state => {
+      return state.categories.length
+    },
+    doneTodos : state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state) => {
+      return state.todos.filter(todo => !todo.done).length
+    }
+  }
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is about page</h1>
+  <div>
+    <h1>Add a new Product</h1>
+    <h3>{{product.name}}</h3>
+    <h3>{{product.id}}</h3>
+    <p>There are mainly {{cateroyLength}} Types of Products</p>
+    <ol>
+      <li v-for="category in categories" :key="category">{{category}}</li>
+    </ol>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: mapState({
+    cateroyLength() {
+      return this.$store.state.categories.length
+    },
+    ...mapState(["categories", "product"])
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
